2b139753cd72e6fd42d69de2cde06224cc324b51
IBNBlank <847235539@qq.com>
2025-11-04 21:17:44 +0800

    feat(v0.1.15)

--- hex_robo_utils/ctrl_util.py
+++ hex_robo_utils/ctrl_util.py
@@ -11,15 +11,20 @@ import numpy as np
 
 class HexCtrlUtilMit:
 
-    def __init__(self, ctrl_limit: np.ndarray):
-        self.__ctrl_upper = ctrl_limit[:, 1]
-        self.__ctrl_lower = ctrl_limit[:, 0]
+    def __init__(self, ctrl_limit: np.ndarray | None = None):
+        if ctrl_limit is not None:
+            self.__ctrl_upper = ctrl_limit.copy()
+            self.__ctrl_lower = -ctrl_limit.copy()
+        else:
+            self.__ctrl_upper = None
+            self.__ctrl_lower = None
 
     def __call__(self, kp, kd, q_tar, dq_tar, q_cur, dq_cur, tau_comp):
         q_err = q_tar - q_cur
         dq_err = dq_tar - dq_cur
-        tau_ctrl = np.clip(kp * q_err + kd * dq_err, self.__ctrl_lower,
-                           self.__ctrl_upper)
+        tau_ctrl = kp * q_err + kd * dq_err
+        if (self.__ctrl_upper is not None) and (self.__ctrl_lower is not None):
+            tau_ctrl = np.clip(tau_ctrl, self.__ctrl_lower, self.__ctrl_upper)
         return tau_ctrl + tau_comp
 
 
@@ -30,13 +35,17 @@ class HexCtrlUtilPid:
         kp: np.ndarray,
         ki: np.ndarray,
         kd: np.ndarray,
-        ctrl_limit: np.ndarray,
+        ctrl_limit: np.ndarray | None = None,
     ):
         self.__kp = kp.copy()
         self.__ki = ki.copy()
         self.__kd = kd.copy()
-        self.__ctrl_upper = ctrl_limit[:, 1]
-        self.__ctrl_lower = ctrl_limit[:, 0]
+        if ctrl_limit is not None:
+            self.__ctrl_upper = ctrl_limit.copy()
+            self.__ctrl_lower = -ctrl_limit.copy()
+        else:
+            self.__ctrl_upper = None
+            self.__ctrl_lower = None
 
         # buffer
         self.__last_ctrl = np.zeros_like(kp)
@@ -52,6 +61,8 @@ class HexCtrlUtilPid:
         d_term = self.__kd * (dq_err - self.__last_dq_err)
         delta_ctrl = p_term + i_term + d_term
         tau_ctrl = self.__last_ctrl + delta_ctrl
+        if (self.__ctrl_upper is not None) and (self.__ctrl_lower is not None):
+            tau_ctrl = np.clip(tau_ctrl, self.__ctrl_lower, self.__ctrl_upper)
 
         self.__last_ctrl = tau_ctrl
         self.__last_q_err = q_err
